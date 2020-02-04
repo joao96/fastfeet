474171f2d0f37506e77c107dd113b17b017bb87a
joao96 <jvpoletti@gmail.com>
2020-02-04 12:07:21 -0300

    :sparkles: new routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
+
 import SessionController from './app/controllers/SessionController';
 
 import authMiddlaware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -17,5 +23,12 @@ routes.put('/users', UserController.update);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+// routes.get('/deliveryman', DeliverymanController.index);
+routes.post('/deliveryman', DeliverymanController.store);
+routes.put('/deliveryman/:id', DeliverymanController.update);
+// routes.delete('/deliveryman/:id', DeliverymanController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
