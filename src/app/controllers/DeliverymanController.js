5aa9909f8e32e81a56b4ec8ce6887ae3c4f7dd3a
joao96 <jvpoletti@gmail.com>
2020-02-04 12:06:49 -0300

    :sparkles: deliveryman feature

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,68 @@
+import * as Yup from 'yup';
+import Deliveryman from '../models/Deliveryman';
+
+class DeliverymanController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const deliveymanExists = await Deliveryman.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (deliveymanExists) {
+      return res.status(400).json({ error: 'Deliveryman already exists.' });
+    }
+
+    const { id, name, email } = await Deliveryman.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      avatar_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const { id } = req.params;
+    const { email } = req.body;
+
+    const deliveryman = await Deliveryman.findByPk(id);
+
+    if (email && email !== deliveryman.email) {
+      const deliverymanExists = await Deliveryman.findOne({ where: { email } });
+      if (deliverymanExists) {
+        return res.status(400).json({ error: 'Deliveryman already exists.' });
+      }
+    }
+
+    const { name, avatar_id } = await deliveryman.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar_id,
+    });
+  }
+}
+
+export default new DeliverymanController();
