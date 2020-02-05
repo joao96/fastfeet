5ca2d4c467dd24830dcd12c6fe2716cac53d9e7b
joao96 <jvpoletti@gmail.com>
2020-02-05 02:31:36 -0300

    :sparkles: inserting problems to deliveries

--- src/database/index.js
+++ src/database/index.js
@@ -5,10 +5,10 @@ import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
 import Deliveryman from '../app/models/Deliveryman';
 import Order from '../app/models/Order';
-
+import DeliveryProblem from '../app/models/DeliveryProblem';
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, Deliveryman, File, Order];
+const models = [User, Recipient, Deliveryman, File, Order, DeliveryProblem];
 
 class Database {
   constructor() {
